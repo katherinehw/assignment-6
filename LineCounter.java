c1868542e75b414eb77f9097b61b6420e25bfcf3
katherinehw <71749102+katherinehw@users.noreply.github.com>
2021-03-19 01:52:30 -0700

    Update LineCounter.java

--- LineCounter.java
+++ LineCounter.java
@@ -1,74 +1,70 @@
 package herman;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
 import java.util.List;
 
 public class LineCounter {
-	
+
 	File fi;
-	int lines = 0;//needs to be outside of method
-	
+	int count = 0;// needs to be outside of method
+
+	public List<String> javaFile;
+
 	public LineCounter(File fi) {
 		this.fi = fi;
 	}
-	
-	public int countLines() throws IOException{
-		
-		List<String> search = Files.readAllLines(Path.of(String.valueOf(this.fi)));//reads file
-		int cnt = 0;
-		
-		for(String pass : search) {//for each string in file
-			
-			cnt++;//counts iterations of for loop
-			
-			if(pass.length() > 0) {//if the word is greater than a space
-				
-				for(int i = 1; i < pass.length(); i++) {//for loop to go through the chars in the indexed word
-					
-					if(pass.charAt(i) == '/' && pass.charAt(i+1) == '/') {break;}//determines if something is commented and will not count them
-					else if(pass.charAt(i) == '*' && pass.charAt(i+1) == '*') {break;}//comment
-					else if(pass.charAt(i) == '*' && pass.charAt(i+1) == '/') {//comment
-						
-						int indexer = search.indexOf(pass);//finds the index of the current word
-						boolean checker = check(search.get(indexer+1));//gets the second word and sends to checker method
-						
-						if(!checker) {lines++;}//if it passes checker it will update the lines counter
-						
-						break;
-						
-					}else if(cnt == pass.length() - 1) {//adds line values if index matches the length of the word
-						
-						lines++;
-						
-					}
-					
-				}
-				
-			}
-			
-		}
-		return lines;
-	}
-		
-	//a checker method to make sure there isnt a corresponding commment mark
-		private boolean check(String str) {
-			
-			for (int i=0; i<str.length(); i++) {
-				
-				if(str.charAt(i) == '*') {
-					return true;
-				}
-				
-			}
-			return false;
-		
-		}
-	
-	
 
+	public int search() throws IOException{
+		
+		javaFile = Files.readAllLines(Path.of(String.valueOf(this.fi)));//reads file
+		
+		for (int z = 0; z<javaFile.size(); z++) {//each line of java file
+			if (javaFile.get(z).trim().length()>0) {//removes blank spaces
+				
+				for(int y = 0; y< javaFile.get(z).length(); y++){//looks for comments and new lines, ignores them
+					
+					if(javaFile.get(z).charAt(y) == '/' && javaFile.get(z).charAt(y+1) == '*') {
+						break; 
+					}
+					else if (javaFile.get(z).charAt(y) == '/' && javaFile.get(z).charAt(y + 1) == '/') {
+						break;
+	                } 
+		             else if (javaFile.get(z).charAt(y) == '*' && javaFile.get(z).charAt(y + 1) != '/') {
+		
+		                boolean check = quickS(javaFile.get(z + 1));
+		                if (!check) {
+		                    count++;//adds new line if *multiplier not comment
+		                }
+		                break;
+		
+		            } else if (javaFile.get(z).charAt(y) == '*' && (javaFile.get(z).charAt(y + 1) == '/')) {
+		                break;
+		            } else if (y == javaFile.get(z).length() - 1) {
+		                count++;
+		            }
+				}
+			}
+		}
+		
+		
+
+		return count;
+	}
+
+
+	private boolean quickS(String s) {
+	
+		for (int i = 0; i < s.length(); i++) {
+		
+		    if (s.charAt(i) == '*') {
+		        return true;
+		    }
+		}
+		return false;
+	}
 }
